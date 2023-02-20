c141034b37b24dbfd46ce750892a1a7f35019f08
Hardik Kapadia <thecoderenroute@gmail.com>
2023-02-20 22:59:21 +0530

    added BaggesTrees

--- 4. Descision Trees/DescisionTree.py
+++ 4. Descision Trees/DescisionTree.py
@@ -1,3 +1,5 @@
+from random import randint
+from typing import List
 import pandas as pd
 import numpy as np
 
@@ -34,8 +36,8 @@ def intelligence_gain(x: pd.Series, y: pd.Series) -> float:
     for i in ind:
         x_i = x[x == i]
         y_i = y.loc[x_i.index]
-        
-        ig -= ((x_i.size/l)*get_entropy(y_i))
+
+        ig -= (x_i.size / l) * get_entropy(y_i)
 
         # entropies.append(get_entropy(y_i))
         # counts.append(x_i.size)
@@ -48,19 +50,28 @@ def intelligence_gain(x: pd.Series, y: pd.Series) -> float:
     return ig
 
 
+def print_tree(node, tabs=0):
+    for i in range(tabs):
+        print("|\t", end="")
+    print(node.feature)
+    for k, v in node.nodes.items():
+        for _ in range(tabs):
+            print("|\t", end="")
+        print(f"|-> {k}:")
+        v.print_tree(tabs + 1)
+        
 class DescisionTreeNode:
-
-    def __init__(self, feature: str, values: list = None):
+    def __init__(self, feature: str, values: List = None):  # type: ignore
 
         self.feature = feature
         self.nodes = {}
 
-        if(values):
+        if values:
             for val in values:
                 self.nodes[val] = None
 
     def get_node(self, value: str):
-        return self.nodes[value]
+        return self.nodes.get(value)
 
     def set_node(self, value: str, node):
         self.nodes[value] = node
@@ -69,28 +80,19 @@ class DescisionTreeNode:
         return len(self.nodes) == 0
 
     def __str__(self):
-        return self.feature+" -> "+str(self.nodes)
+        return self.feature + " -> " + str(self.nodes)
 
-    def print_tree(self, tabs=0):
-
-        for i in range(tabs):
-            print("|\t", end="")
-
-        print(self.feature)
-        for k, v in self.nodes.items():
-            for _ in range(tabs):
-                print("|\t", end="")
-            print(f"|-> {k}:")
-            v.print_tree(tabs+1)
-            
-
-
-def get_descision_tree(current_df: pd.DataFrame, target_data: pd.Series, depth: int = 0, max_depth: int = 10, ig_limit: float = 0.001):
-
+def get_descision_tree(
+    current_df: pd.DataFrame,
+    target_data: pd.Series,
+    depth: int = 0,
+    max_depth: int = 10,
+    ig_limit: float = 0.001,
+):
     cols = current_df.columns.tolist()
 
     max_ig = -1
-    selected_col = None
+    selected_col: str = ""
 
     for col in cols:
 
@@ -101,7 +103,7 @@ def get_descision_tree(current_df: pd.DataFrame, target_data: pd.Series, depth: 
             selected_col = col
 
     if max_ig <= ig_limit or depth >= max_depth:
-        return DescisionTreeNode(target_data.value_counts().idxmax())
+        return DescisionTreeNode(str(target_data.value_counts().idxmax()))
 
     vals = current_df[selected_col].value_counts().index.tolist()
 
@@ -111,15 +113,15 @@ def get_descision_tree(current_df: pd.DataFrame, target_data: pd.Series, depth: 
 
         temp_df = current_df[current_df[selected_col] == val]
         temp_target_data = target_data.loc[temp_df.index]
-        temp_node = get_descision_tree(temp_df, temp_target_data, depth+1)
+        temp_df_2 = temp_df.drop(selected_col, axis=1)
+        temp_node = get_descision_tree(temp_df_2, temp_target_data, depth + 1)
         node.set_node(val, temp_node)
 
     return node
 
 
 class DescisionTree:
-
-    def __init__(self, training_X=None, training_y=None):
+    def __init__(self, training_X: pd.DataFrame, training_y: pd.Series):
         self.training_X = training_X
         self.training_y = training_y
         self.node = None
@@ -134,7 +136,7 @@ class DescisionTree:
 
     def predict(self, feature_vector):
 
-        if(not self.node):
+        if not self.node:
             raise SyntaxError("Please train the model first")
 
         temp = self.node
@@ -143,14 +145,14 @@ class DescisionTree:
 
             t = temp.get_node(feature_vector[temp.feature])
 
-            if(t.is_leaf()):
+            if t.is_leaf():
                 return t.feature
 
             temp = t
 
     def test(self, testing_X, testing_Y):
-        
-        if(not self.node):
+
+        if not self.node:
             raise SyntaxError("Please train the model first")
 
         correct = 0
@@ -159,7 +161,7 @@ class DescisionTree:
         for i in testing_X.index:
             t = self.predict(testing_X.loc[i])
 
-            if(t == testing_Y.loc[i]):
+            if t == testing_Y.loc[i]:
                 correct += 1
             else:
                 wrong += 1
@@ -167,4 +169,102 @@ class DescisionTree:
         return (100.0 * correct) / (correct + wrong)
 
     def view_tree(self):
-        self.node.print_tree()
+        print_tree(self.node)
+
+
+class BaggedDecisionTree:
+    def __init__(
+        self, training_X: pd.DataFrame, training_y: pd.Series, trees: int = 32
+    ):
+        self.training_X = training_X
+        self.training_y = training_y
+        self.trees = trees
+        self.nodes = []
+
+    def fit(self, training_X, training_y):
+        self.training_X = training_X
+        self.training_y = training_y
+        self.nodes = []
+
+    def get_sampled_data(self):
+
+        n = self.training_y.size
+
+        choices = [randint(0, n * 3) % n for _ in range(n)]
+
+        sampled_X = self.training_X.iloc[choices]
+        sampled_Y = self.training_y.iloc[choices]
+
+        sampled_X.reset_index(drop=True, inplace=True)
+        sampled_Y.reset_index(drop=True, inplace=True)
+
+        return (sampled_X, sampled_Y)
+
+    def train(self):
+
+        for _ in range(self.trees):
+
+            temp_train_X, temp_train_Y = self.get_sampled_data()
+
+            self.nodes.append(get_descision_tree(temp_train_X, temp_train_Y))
+
+    def predict(self, feature_vector):
+
+        if len(self.nodes) == 0:
+            raise SyntaxError("Please train the model first")
+
+        votes = {}
+
+        for node in self.nodes:
+
+            vote = self.predict_single_tree(feature_vector, node)
+            if vote:
+                val = votes.get(vote, 0) + 1
+                votes[vote] = val
+
+        n = len(self.nodes)
+
+        max_votes = -1
+        highest = None
+
+        for k, v in votes.items():
+
+            if v > max_votes:
+                highest = k
+                max_votes = v
+
+        return (highest, votes.get(highest) / n)
+
+    def predict_single_tree(self, feature_vector, node):
+
+        temp = node
+
+        while True:
+
+            t = temp.get_node(feature_vector[temp.feature])
+
+            if t == None:
+                return None
+
+            if t.is_leaf():
+                return t.feature
+
+            temp = t
+
+    def test(self, testing_X, testing_Y):
+
+        if len(self.nodes) == 0:
+            raise SyntaxError("Please train the model first")
+
+        correct = 0
+        wrong = 0
+
+        for i in testing_X.index:
+            t = self.predict(testing_X.loc[i])
+
+            if t[0] == testing_Y.loc[i]:
+                correct += 1
+            else:
+                wrong += 1
+
+        return (100.0 * correct) / (correct + wrong)
