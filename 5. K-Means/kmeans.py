e868b15a7882b97a436877f6dad7f3cb36205fae
Hardik Kapadia <thecoderenroute@gmail.com>
2023-02-25 18:37:29 +0530

    Image compression with k means

--- 5. K-Means/kmeans.py
+++ 5. K-Means/kmeans.py
@@ -1,8 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def get_distance_between(x: np.ndarray, y: np.ndarray):
-    return np.linalg.norm(x, y)
+    return np.linalg.norm(x-y)
 
 
 def compute_centroid_for_points(points: np.ndarray):
@@ -18,7 +19,8 @@ class KMeans:
         self.data = training_data
         self.n = training_data.shape[0]
         self.dimension = training_data.shape[1]
-        self.centroids = np.array()
+        self.centroids = np.empty(self.n)
+        self.training_classes = np.empty(self.n)
 
     # randomly intialize centroids
     def get_random_initializations(self, K):
@@ -47,8 +49,9 @@ class KMeans:
                 get_distance_between(self.data[i], centroids[centroid_classes[i]])
             )
 
-        print(f"loss is: {loss}")
         loss /= self.n
+        
+        return loss
 
     # train to get a single cluster point
     def _train_single(
@@ -56,16 +59,16 @@ class KMeans:
         K: int,
         previous_centroids: np.ndarray = None,
         centroid_classes: np.ndarray = None,
-        min_loss: float = 1,
+        min_loss: float = 0.002,
     ):
 
-        if not previous_centroids:
+        if previous_centroids is None:
             previous_centroids = self.get_random_initializations(K)
 
-        if not centroid_class:
-            centroid_class = np.zeros(self.n, dtype=int)
+        if centroid_classes is None:
+            centroid_classes = np.zeros(self.n, dtype=int)
 
-        for i in range(self.data):
+        for i in range(self.data.shape[0]):
 
             training_set = self.data[i]
 
@@ -77,36 +80,56 @@ class KMeans:
                 temp_dist = get_distance_between(previous_centroids[k], training_set)
 
                 if temp_dist < closest_dist:
-                    closest = K
+                    closest = k
                     closest_dist = temp_dist
 
-            centroid_class[i] = closest
+            centroid_classes[i] = closest
 
         for k in range(K):
-            points = self.data[centroid_class == k]
+            points = self.data[centroid_classes == k]
             previous_centroids[k] = compute_centroid_for_points(points)
 
-        current_loss = self.compute_loss(previous_centroids, centroid_class)
+        current_loss = self.compute_loss(previous_centroids, centroid_classes)
+        print(f'current loss: {current_loss}')
 
         if current_loss <= min_loss:
-            return (previous_centroids, current_loss)
+            return (previous_centroids, current_loss, centroid_classes)
         else:
-            return self._train_single(K, previous_centroids, centroid_class, min_loss)
+            return self._train_single(K, previous_centroids, centroid_classes, min_loss)
 
     # train for all iterations
-    def train(self, clusters: int, iterations: int = 100):
+    def train(self, clusters: int, iterations: int = 100, min_loss: int = 0.002):
 
-        if not self.centroids:
+        if self.centroids is None:
             raise ValueError("Please fit data first")
 
         min_loss = 1000000000
         f_centroids = None
+        temp_classes = None
 
-        for i in range(iterations):
+        for _ in range(iterations):
+            
+            print('\ntrying for new model now.')
 
-            temp_centroids, loss = self._train_single(clusters)
+            temp_centroids, loss, classes = self._train_single(clusters,min_loss=min_loss)
 
             if loss < min_loss:
                 f_centroids = temp_centroids
+                temp_classes = classes
 
-        return f_centroids
+        self.centroids = f_centroids
+        self.training_classes = temp_classes
+
+    # predict for a single instance
+    def predict(self, instance: np.ndarray):
+
+        closest = 0
+        closest_dist = get_distance_between(instance, self.centroids[0])
+
+        for k in range(1, self.centroids.shape[0]):
+            temp_dist = get_distance_between(self.centroids[k], instance)
+            if temp_dist < closest_dist:
+                closest = k
+                closest_dist = temp_dist
+
+        return self.centroids[closest]
